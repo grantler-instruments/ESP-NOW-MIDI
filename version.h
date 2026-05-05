016585d74b1e2e8f586cb0809c0619deb390d534
Thomas Geissl <thomas.geissl@gmail.com>
2026-05-05 09:16:14 +0200

    remove arduino dependency

--- version.h
+++ version.h
@@ -3,8 +3,22 @@
 #define ESP_NOW_MIDI_VERSION_MINOR 10
 #define ESP_NOW_MIDI_VERSION_PATCH 9
 
-inline String getVersion() {
-    return String(ESP_NOW_MIDI_VERSION_MAJOR) + "." + 
-           String(ESP_NOW_MIDI_VERSION_MINOR) + "." + 
-           String(ESP_NOW_MIDI_VERSION_PATCH);
+
+#define ENOMIK_STR_HELPER(x) #x
+#define ENOMIK_STR(x) ENOMIK_STR_HELPER(x)
+#define ESP_NOW_MIDI_VERSION_STR \
+    ENOMIK_STR(ESP_NOW_MIDI_VERSION_MAJOR) "." \
+    ENOMIK_STR(ESP_NOW_MIDI_VERSION_MINOR) "." \
+    ENOMIK_STR(ESP_NOW_MIDI_VERSION_PATCH)
+
+
+inline const char* getVersion() {
+    return ESP_NOW_MIDI_VERSION_STR;
 }
+
+#ifdef ARDUINO
+#include <Arduino.h>
+inline String getVersionString() {
+    return String(getVersion());
+}
+#endif
