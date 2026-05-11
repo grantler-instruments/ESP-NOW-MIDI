a2e2c66017819a10f05149bae9de142814f2f62c
Thomas Geissl <thomas.geissl@gmail.com>
2026-05-11 20:16:40 +0200

    bumped minor version

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #pragma once
 #define ESP_NOW_MIDI_VERSION_MAJOR 0
-#define ESP_NOW_MIDI_VERSION_MINOR 10
-#define ESP_NOW_MIDI_VERSION_PATCH 9
+#define ESP_NOW_MIDI_VERSION_MINOR 11
+#define ESP_NOW_MIDI_VERSION_PATCH 0
 
 inline String getVersion() {
     return String(ESP_NOW_MIDI_VERSION_MAJOR) + "." + 
