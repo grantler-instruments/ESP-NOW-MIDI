86f2d5b9789e4695944b5f7f74f2aac47bc8ed8b
Thomas Geissl <thomas.geissl@gmail.com>
2026-06-05 11:37:37 +0200

    fixed version

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 #pragma once
 #define ESP_NOW_MIDI_VERSION_MAJOR 0
-#define ESP_NOW_MIDI_VERSION_MINOR 12
+#define ESP_NOW_MIDI_VERSION_MINOR 11
 #define ESP_NOW_MIDI_VERSION_PATCH 0
 
 inline String getVersion() {
