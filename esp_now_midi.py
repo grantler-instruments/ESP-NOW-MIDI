39d41dd7f88b8fcd736b19e14edca37960c81d61
Thomas Geissl <thomas.geissl@gmail.com>
2026-06-23 06:21:55 +0100

    bumped version

--- esp_now_midi.py
+++ esp_now_midi.py
@@ -9,7 +9,7 @@ import espnow
 # Keep in sync with version.h
 VERSION_MAJOR = 0
 VERSION_MINOR = 12
-VERSION_PATCH = 1
+VERSION_PATCH = 2
 VERSION = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}"
 
 
