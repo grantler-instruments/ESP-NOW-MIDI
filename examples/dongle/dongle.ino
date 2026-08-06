716b6351f7aab9bef61f88048048df5541c0698e
Thomas Geissl <thomas.geissl@gmail.com>
2026-08-06 15:12:24 +0200

    bumped minor version

--- examples/dongle/dongle.ino
+++ examples/dongle/dongle.ino
@@ -19,10 +19,6 @@ void setup() {
   _dongle.setDisplay(&display);
 #endif
 
-  // Optional: customize USB identity before begin()
-  // _dongle.setManufacturerDescriptor("grantler instruments");
-  // _dongle.setProductDescriptor("enomik3000_dongle");
-
   // Optional: hard-add a known peer (or wait for auto-discovery)
   // uint8_t clientMac[6] = { 0x84, 0xF7, 0x03, 0xF2, 0x54, 0x62 };
   // _dongle.addPeer(clientMac);
